960dd44c87faf0f801b95bc788f114efdea9866a
Angel Hernández <macmini@Angels-Mac-mini-2.local>
2025-05-13 22:49:43 -0400

    fix: input.tsx tipado estricto y verificar-expediente corregido

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 // next.config.js
-const path = require("path");
+const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   webpack(config) {
-    config.resolve.alias["@"] = path.resolve(__dirname, "src");
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
     return config;
-  },
+  }
 };
 
 module.exports = nextConfig;
